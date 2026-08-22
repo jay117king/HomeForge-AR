f88acb9dd736c81be7a514ec5a7b7d935ca48e37
jay117king <138710811+jay117king@users.noreply.github.com>
2026-08-22 17:49:24 +0200

    Fix Android CI: proper SDK setup with android-actions, Gradle 8.9, Compose compiler plugin, local.properties

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
     id("com.google.devtools.ksp")
     id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21"
 }
@@ -48,10 +49,6 @@ android {
     buildFeatures {
         compose = true
         buildConfig = true
-    }
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.15"
     }
 
     packaging {
